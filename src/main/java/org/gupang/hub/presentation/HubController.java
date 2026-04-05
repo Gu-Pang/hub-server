df97e2b9608e83d23a447a64c46871a964c4c7cf
ddangme <ssilver121@naver.com>
2026-04-06 02:20:27 +0900

    refactor: presentation dto 패키지 구조 변경
    - presentation dto 패캐지를 request와 response 패키지로 분리했습니다.

--- src/main/java/org/gupang/hub/presentation/HubController.java
+++ src/main/java/org/gupang/hub/presentation/HubController.java
@@ -4,15 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.gupang.hub.application.hub.dto.HubDetailInfo;
 import org.gupang.hub.application.hub.dto.HubInfo;
 import org.gupang.hub.application.hub.service.HubService;
-import org.gupang.hub.presentation.dto.HubDetailResponse;
-import org.gupang.hub.presentation.dto.HubResponse;
+import org.gupang.hub.presentation.dto.response.HubDetailResponse;
+import org.gupang.hub.presentation.dto.response.HubResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
